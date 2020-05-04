722ba8d04b47df6fe09d684da0241620ab70c7a0
Kilian <30512397+Void156@users.noreply.github.com>
2020-05-04 22:56:09 +0100

    initial commit

---
+++ weather.py
@@ -0,0 +1,35 @@
+import requests
+import json
+import discord
+from discord.ext import commands
+#Settings
+locationiq = ""
+openweather = ""
+discordtoken = ""
+
+bot = commands.Bot(command_prefix='$', description='A weather reporting bot.')
+
+@bot.command()
+async def weather(ctx, a: str):
+    lociqreq = requests.get("https://eu1.locationiq.com/v1/search.php?key=" + locationiq + "&q=" + a + "&format=json&limit=1")
+    json_data = json.loads(lociqreq.text)
+    lat = json_data[0]["lat"]
+    lon = json_data[0]["lon"]
+
+    openweathreq = requests.get("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon +"&appid=" + openweather + "&units=metric")
+    json_data1 = json.loads(openweathreq.text)
+    temp = json_data1["current"]["temp"]
+    conditions = json_data1["current"]["weather"][0]["description"]
+    humidity = json_data1["current"]["humidity"]
+    pressure = json_data1["current"]["pressure"]
+    visibility = json_data1["current"]["visibility"]
+
+    embed = discord.Embed(title=a, color=0xeee657)
+    embed.add_field(name="Temperature", value="" + str(temp) + " Celcius", inline=False)
+    embed.add_field(name="Current", value="" + str(conditions), inline=False)
+    embed.add_field(name="Humidity", value="" + str(humidity) + "%", inline=False)
+    embed.add_field(name="Pressure", value="" + str(pressure) + " hPa", inline=False)
+    embed.add_field(name="Visibility", value="" + str(visibility) + " Meters", inline=False)
+    await ctx.send(embed=embed)
+
+bot.run(discordtoken)
