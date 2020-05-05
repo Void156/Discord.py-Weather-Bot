4b2e4cbefdf60787448b528bb2ecde8e3b86756d
Kilian <30512397+Void156@users.noreply.github.com>
2020-05-05 17:01:54 +0100

    Addded Invalid Response Message.

--- weather.py
+++ weather.py
@@ -24,25 +24,28 @@ async def weather(ctx, a: str):
         async with  session.get("https://eu1.locationiq.com/v1/search.php?key=" + locationiq + "&q=" + a + "&format=json&limit=1") as resploc:
             data = await resploc.text()
             json_data = json.loads(data)
-            lat = json_data[0]["lat"]
-            lon = json_data[0]["lon"]
+            try:
+                lat = json_data[0]["lat"]
+                lon = json_data[0]["lon"]
+                async with session.get("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon +"&appid=" + openweather + "&units=metric") as respope:
+                    data1 = await respope.text()
+                    json_data1 = json.loads(data1)
+                    temp = json_data1["current"]["temp"]
+                    conditions = json_data1["current"]["weather"][0]["description"]
+                    humidity = json_data1["current"]["humidity"]
+                    pressure = json_data1["current"]["pressure"]
+                    visibility = json_data1["current"]["visibility"]
 
-        async with session.get("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon +"&appid=" + openweather + "&units=metric") as respope:
-            data1 = await respope.text()
-            json_data1 = json.loads(data1)
-            temp = json_data1["current"]["temp"]
-            conditions = json_data1["current"]["weather"][0]["description"]
-            humidity = json_data1["current"]["humidity"]
-            pressure = json_data1["current"]["pressure"]
-            visibility = json_data1["current"]["visibility"]
-
-        embed = discord.Embed(title=a, color=0x57eee6)
-        embed.add_field(name="Temperature", value="" + str(temp) + " Celcius", inline=False)
-        embed.add_field(name="Current", value="" + str(conditions), inline=False)
-        embed.add_field(name="Humidity", value="" + str(humidity) + "%", inline=False)
-        embed.add_field(name="Pressure", value="" + str(pressure) + " hPa", inline=False)
-        embed.add_field(name="Visibility", value="" + str(visibility) + " Meters", inline=False)
-        embed.set_footer(text="Weatherly DEVELOPMENT VERSION", icon_url="https://library.kissclipart.com/20180917/csw/kissclipart-weather-icon-clipart-weather-rain-clip-art-77feae16d88a32d1.png")
-        await ctx.send(embed=embed)
+                embed = discord.Embed(title=a, color=0x57eee6)
+                embed.add_field(name="Temperature", value="" + str(temp) + " Celcius", inline=False)
+                embed.add_field(name="Current", value="" + str(conditions), inline=False)
+                embed.add_field(name="Humidity", value="" + str(humidity) + "%", inline=False)
+                embed.add_field(name="Pressure", value="" + str(pressure) + " hPa", inline=False)
+                embed.add_field(name="Visibility", value="" + str(visibility) + " Meters", inline=False)
+                embed.set_footer(text="Weatherly DEVELOPMENT VERSION", icon_url="https://library.kissclipart.com/20180917/csw/kissclipart-weather-icon-clipart-weather-rain-clip-art-77feae16d88a32d1.png")
+                await ctx.send(embed=embed)
+            except KeyError:
+                await ctx.send("Error, invalid city.")
+       
 
 bot.run(discordtoken)
