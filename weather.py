a9cbf612be26175cbdafc1a75843fdb7587b7f90
Kilian <30512397+Void156@users.noreply.github.com>
2020-05-05 10:20:28 +0100

    Updated embed, colour and settings
    Added branding to embed footer added prefix to settings page and changed the colour code of the embed.

--- weather.py
+++ weather.py
@@ -6,8 +6,9 @@ from discord.ext import commands
 locationiq = ""
 openweather = ""
 discordtoken = ""
+prefix = "$"
 
-bot = commands.Bot(command_prefix='$', description='A weather reporting bot.')
+bot = commands.Bot(command_prefix='' + prefix, description='A weather reporting bot.')
 
 @bot.command()
 async def weather(ctx, a: str):
@@ -24,13 +25,13 @@ async def weather(ctx, a: str):
     pressure = json_data1["current"]["pressure"]
     visibility = json_data1["current"]["visibility"]
 
-    embed = discord.Embed(title=a, color=0xeee657)
+    embed = discord.Embed(title=a, color=0x57eee6)
     embed.add_field(name="Temperature", value="" + str(temp) + " Celcius", inline=False)
     embed.add_field(name="Current", value="" + str(conditions), inline=False)
     embed.add_field(name="Humidity", value="" + str(humidity) + "%", inline=False)
     embed.add_field(name="Pressure", value="" + str(pressure) + " hPa", inline=False)
     embed.add_field(name="Visibility", value="" + str(visibility) + " Meters", inline=False)
-    embed.set_footer(text="Void#1123's WeatherBot", icon_url="https://library.kissclipart.com/20180917/csw/kissclipart-weather-icon-clipart-weather-rain-clip-art-77feae16d88a32d1.png")
+    embed.set_footer(text="Weatherly BOT", icon_url="https://library.kissclipart.com/20180917/csw/kissclipart-weather-icon-clipart-weather-rain-clip-art-77feae16d88a32d1.png")
     await ctx.send(embed=embed)
 
 bot.run(discordtoken)
